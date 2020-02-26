7229e54c678c1acb9f1897926bc69546ba260211
Tharindu Ranaweera <tharindu.ranaweera@syscolabs.com>
2020-02-26 12:09:55 +0530

    move reducer behavior to functions

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -19,7 +19,8 @@ const initialState = Map({
   todoDescriptionWarningText: null
 });
 
-const todoReducer = (state = initialState, action) => {
+const todoReducer = (action, state = initialState) => {
+  const {value} = action;
   switch (action.type) {
     case 'MARK_AS_COMPLETE':
       const completedItemIndex = state
@@ -32,30 +33,47 @@ const todoReducer = (state = initialState, action) => {
       );
 
     case ON_CLICK_ADD_TODO_BUTTON:
-      return state
-        .set('isAddTodoInputVisible', !state.get('isAddTodoInputVisible'))
-        .set('todoDescription', '');
+      return onClickAddTodoButton(state);
 
     case ON_CLICK_SAVE_TODO_BUTTON:
-      return state.set('saveTodoButtonText', 'Saving...');
+      return onClickSaveTodoButton(state);
 
     case ON_CHANGE_TODO_DESCRIPTION:
-      return state.set('todoDescription', action.value);
+      return  onChangeTodoDescription(state, value);
 
     case ON_SAVE_TODO_SUCCESS:
-      return state
-        .updateIn(['todos'], todos =>
-          todos.push({
-            id: uuid(),
-            description: action.value,
-            isComplete: false
-          })
-        )
-        .set('saveTodoButtonText', 'Save')
-        .set('todoDescription', '');
+      return onSaveTodoSuccess(state, value);
+
     default:
       return state;
   }
+};
+
+const onClickAddTodoButton = state => {
+  return state
+    .set('isAddTodoInputVisible', !state.get('isAddTodoInputVisible'))
+    .set('todoDescription', '');
+};
+
+const onClickSaveTodoButton = state => {
+  return state.set('saveTodoButtonText', 'Saving...');
+};
+
+const onChangeTodoDescription = (state, value) => {
+  return state.set('todoDescription', value);
+};
+
+const onSaveTodoSuccess = (state, value) => {
+  return state
+    .updateIn(['todos'], todos =>
+      todos.push({
+        id: uuid(),
+        description: value,
+        isComplete: false
+      })
+    )
+    .set('saveTodoButtonText', 'Save')
+    .set('todoDescription', '');
 };
 
 export default todoReducer;
